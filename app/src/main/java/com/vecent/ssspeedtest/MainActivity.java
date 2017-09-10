d4bdd04603f93c3da020d2b748d75fe57fcfac80
luckzhiwei <lzw00000@qq.com>
2017-09-10 19:32:20 +0800

    prepare for ping thread pool

--- app/src/main/java/com/vecent/ssspeedtest/MainActivity.java
+++ app/src/main/java/com/vecent/ssspeedtest/MainActivity.java
@@ -1,22 +1,16 @@
 package com.vecent.ssspeedtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.vecent.ssspeedtest.model.INetImpl;
-import com.vecent.ssspeedtest.model.SpeedTest;
-import com.vecent.ssspeedtest.util.Constant;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.vecent.ssspeedtest.controller.SpeedTestActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button testSpeedBtn;
-    private SpeedTest mSpeedTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +25,12 @@ public class MainActivity extends AppCompatActivity {
         this.testSpeedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mSpeedTest.startTest(new INetImpl());
+                startActivity(new Intent(getApplicationContext(), SpeedTestActivity.class));
             }
         });
     }
 
     private void initModel() {
-        ArrayList<String> servers = new ArrayList<>();
-        servers.add("taobao.com");
-        servers.add("baidu.com");
-        servers.add("www.google.com.hk");
-        this.mSpeedTest = new SpeedTest(servers);
+
     }
 }
