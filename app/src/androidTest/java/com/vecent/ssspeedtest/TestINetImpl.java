70bb6bdd5d25cc99f79595f8b223984c7a94a8cb
luckzhiwei <lzw00000@qq.com>
2017-09-17 10:33:02 +0800

    add the android test for httprequest

--- app/src/androidTest/java/com/vecent/ssspeedtest/TestINetImpl.java
+++ app/src/androidTest/java/com/vecent/ssspeedtest/TestINetImpl.java
@@ -1,10 +1,14 @@
 package com.vecent.ssspeedtest;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
 import com.vecent.ssspeedtest.model.INet;
 import com.vecent.ssspeedtest.model.INetImpl;
 import com.vecent.ssspeedtest.model.bean.SpeedTestResult;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -13,15 +17,61 @@ import org.junit.Test;
 
 public class TestINetImpl {
 
+    private Context appContext;
+
+    @Before
+    public void before() {
+        this.appContext = InstrumentationRegistry.getTargetContext();
+    }
+
     @Test
     public void testINetImplPingWhiteListAddr() {
         INet iNetForTest = new INetImpl();
-        SpeedTestResult result = iNetForTest.httpRequest("taobao.com");
+        SpeedTestResult result = iNetForTest.getHttpTestResult("https://www.taobao.com/");
+
+        Assert.assertEquals(false, result.isExceptionOccured());
+        Assert.assertEquals(false, result.isRedirect());
+        Assert.assertEquals(false, result.isTimedOut());
+        Assert.assertEquals(false, result.isUrlWrong());
+
+        Assert.assertEquals(200, result.getStatusCode());
+        Assert.assertEquals(true, result.getTotalSize() > 0);
+        Assert.assertEquals(true, result.getTimeUsed() > 0);
+        Assert.assertEquals(true, result.getDownLoadSpeed() > 0);
+
     }
 
     @Test
     public void testINetImplPingBlackListAddr() {
         INet iNetForTest = new INetImpl();
-        SpeedTestResult result = iNetForTest.httpRequest("www.google.com.hk");
+        SpeedTestResult result = iNetForTest.getHttpTestResult("https://www.google.com.hk");
+
+        Assert.assertEquals(true, result.isExceptionOccured());
+        Assert.assertEquals(false, result.isRedirect());
+        Assert.assertEquals(true, result.isTimedOut());
+        Assert.assertEquals(false, result.isUrlWrong());
+
+        Assert.assertEquals(0, result.getTotalSize());
+        Assert.assertEquals(0, result.getTimeUsed());
+        Assert.assertEquals(0, result.getDownLoadSpeed());
+        Assert.assertEquals(0, result.getStatusCode());
+    }
+
+
+    @Test
+    public void testINetImplHtpRequestRedirectAddr() {
+        INet iNetForTest = new INetImpl();
+        SpeedTestResult result = iNetForTest.getHttpTestResult("http://www.baidu.com");
+
+        Assert.assertEquals(false, result.isExceptionOccured());
+        Assert.assertEquals(true, result.isRedirect());
+        Assert.assertEquals(false, result.isTimedOut());
+        Assert.assertEquals(false, result.isUrlWrong());
+
+        Assert.assertEquals(200, result.getStatusCode());
+        Assert.assertEquals(true, result.getTotalSize() > 0);
+        Assert.assertEquals(true, result.getTimeUsed() > 0);
+        Assert.assertEquals(true, result.getDownLoadSpeed() > 0);
+
     }
 }
