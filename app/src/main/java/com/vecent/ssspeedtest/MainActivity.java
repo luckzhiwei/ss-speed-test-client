b1f67e1f8c07a1ef2761adfd5a0f6bf4b8b7b01e
luckzhiwei <lzw00000@qq.com>
2017-11-09 19:53:44 +0800

    m:完成删除和显示为空的情况

--- app/src/main/java/com/vecent/ssspeedtest/MainActivity.java
+++ app/src/main/java/com/vecent/ssspeedtest/MainActivity.java
@@ -6,8 +6,10 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.vecent.ssspeedtest.adpater.SSServerAdapter;
 import com.vecent.ssspeedtest.controller.InputSSServerSettingActivity;
@@ -21,10 +23,14 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
-    private ListView serverList;
+    private ListView contentListView;
     private Handler mHandler;
     private ImageView addServerImg;
     private SSServerAdapter adapter;
+    private List<SSServer> ssServerList;
+    private TextView pleaseAddTextView;
+    private ImageView pleaseAddImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +40,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        this.serverList = (ListView) this.findViewById(R.id.common_list_view);
+        this.contentListView = (ListView) this.findViewById(R.id.common_list_view);
         this.addServerImg = (ImageView) this.findViewById(R.id.add_ss_server_img);
+        this.pleaseAddTextView = (TextView) this.findViewById(R.id.plead_add_textview);
+        this.pleaseAddImageView = (ImageView) this.findViewById(R.id.plead_add_img);
         this.addServerImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), InputSSServerSettingActivity.class));
+            }
+        });
+        this.contentListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                deleteServer(ssServerList.get(i));
+                return true;
             }
         });
     }
@@ -48,27 +63,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         if (mHandler == null) mHandler = new Handler(Looper.getMainLooper());
-        lodaData();
+        loadData();
     }
 
-    private void lodaData() {
+    private void loadData() {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 DaoSession daoSession = DaoManager.getInstance(getApplicationContext()).getDaoSession();
-                final List<SSServer> ssServerList = daoSession.getSSServerDao().loadAll();
+                ssServerList = daoSession.getSSServerDao().loadAll();
+
                 mHandler.post(new Runnable() {
                     @Override
                     public void run() {
                         adapter = new SSServerAdapter(MainActivity.this,
                                 R.layout.ss_server_item_layout, ssServerList);
-                        serverList.setAdapter(adapter);
-
+                        contentListView.setAdapter(adapter);
+                        pleaseAddImageView.setVisibility(View.GONE);
+                        pleaseAddTextView.setVisibility(View.GONE);
+                        if (ssServerList.size() == 0) {
+                            pleaseAddImageView.setVisibility(View.VISIBLE);
+                            pleaseAddTextView.setVisibility(View.VISIBLE);
+                        }
                     }
                 });
             }
         }).start();
     }
+
+    private void deleteServer(SSServer server) {
+        DaoSession daoSession = DaoManager.getInstance(getApplicationContext()).getDaoSession();
+        daoSession.getSSServerDao().delete(server);
+        loadData();
+    }
+
 
     @Override
     protected void onDestroy() {
