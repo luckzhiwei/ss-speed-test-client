1bbc521a44fc56d3339b8361eb7dd3eb6c6c0880
luckzhiwei <lzw00000@qq.com>
2018-01-04 19:25:48 +0800

    m:准备写速度时间限制

--- app/src/main/java/com/vecent/ssspeedtest/MainActivity.java
+++ app/src/main/java/com/vecent/ssspeedtest/MainActivity.java
@@ -33,9 +33,7 @@ import com.vecent.ssspeedtest.controller.SpeedTestActivity;
 import com.vecent.ssspeedtest.dao.DaoManager;
 import com.vecent.ssspeedtest.dao.SSServer;
 import com.vecent.ssspeedtest.greendao.DaoSession;
-import com.vecent.ssspeedtest.model.bean.TotalSpeedTestResult;
 import com.vecent.ssspeedtest.service.SpeedTestService;
-import com.vecent.ssspeedtest.util.LogUtil;
 import com.vecent.ssspeedtest.view.EditSSServerSettingDialog;
 
 import java.util.List;
